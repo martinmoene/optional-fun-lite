adc6e716f78e7b60e44909bf71bf5ca429332227
Martin Moene <martin.moene@gmail.com>
2018-09-13 12:06:30 +0200

    Add GNUC -Wconversion, -Wsign-conversion warnings

--- test/tg.bat
+++ test/tg.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set std=%1
 @if "%std%"=="" set std=c++11
-g++ -std=%std% -O2 -Wall -Wextra -Wno-unused-parameter -Doptfun_OPTIONAL_HEADER=\"../../optional-lite/include/nonstd/optional.hpp\" -I../include/nonstd optional-fun-main.t.cpp optional-fun.t.cpp && optional-fun-main.t.exe
+g++ -std=%std% -O2 -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion -Wno-unused-parameter -Doptfun_OPTIONAL_HEADER=\"../../optional-lite/include/nonstd/optional.hpp\" -I../include/nonstd optional-fun-main.t.cpp optional-fun.t.cpp && optional-fun-main.t.exe
 @endlocal
 
