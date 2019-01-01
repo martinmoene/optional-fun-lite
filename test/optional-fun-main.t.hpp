76d5233c32df39673b2e9080ca48a310463c7bdd
Martin Moene <martin.moene@gmail.com>
2019-01-01 11:19:45 +0100

    Add cmake installation and config-file packaging
    Let tests use optional-fun-lite interface library

--- test/optional-fun-main.t.hpp
+++ test/optional-fun-main.t.hpp
@@ -2,7 +2,7 @@
 //
 // https://github.com/martinmoene/optional-lite
 //
-// Distributed under the Boost Software License, Version 1.0. 
+// Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
@@ -11,7 +11,21 @@
 #define TEST_OPTIONAL_FUN_LITE_H_INCLUDED
 
 #include optfun_OPTIONAL_HEADER
-#include "optional-fun.hpp"
+#include "nonstd/optional-fun.hpp"
+
+// Compiler warning suppression for usage of lest:
+
+#ifdef __clang__
+# pragma clang diagnostic ignored "-Wstring-conversion"
+# pragma clang diagnostic ignored "-Wunused-parameter"
+# pragma clang diagnostic ignored "-Wunused-template"
+# pragma clang diagnostic ignored "-Wunused-function"
+# pragma clang diagnostic ignored "-Wunused-member-function"
+#elif defined __GNUC__
+# pragma GCC   diagnostic ignored "-Wunused-parameter"
+# pragma GCC   diagnostic ignored "-Wunused-function"
+#endif
+
 #include "lest_cpp03.hpp"
 
 #define CASE( name ) lest_CASE( specification(), name )
